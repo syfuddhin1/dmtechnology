a096b4b988dc1ad4a881d05fd7f7864539dad41a
soab42 <syfuddhin@gmail.com>
2024-09-08 10:02:35 +0600

    fix date consfusion

--- utils/getStartDate.js
+++ utils/getStartDate.js
@@ -10,14 +10,14 @@ export function getStartDates(inputDate) {
     // If the current month is July (6) or later, fiscal year is current year - next year
     fiscalYearStart = new Date(date.getFullYear(), 6, 1);
     prevYearStart = new Date(date.getFullYear() - 1, 6, 1);
-    prevYearEnd = new Date(date.getFullYear(), 6, 1);
+    prevYearEnd = new Date(date.getFullYear(), 5, 30);
     fiscalYear = `${date.getFullYear()}-${date.getFullYear() + 1}`;
     prevFiscalYear = `${date.getFullYear() - 1}-${date.getFullYear()}`;
   } else {
     // If the current month is before July, fiscal year is last year - current year
     fiscalYearStart = new Date(date.getFullYear() - 1, 6, 1);
     prevYearStart = new Date(date.getFullYear() - 2, 6, 1);
-    prevYearEnd = new Date(date.getFullYear() - 1, 5, 1);
+    prevYearEnd = new Date(date.getFullYear() - 1, 5, 30);
     fiscalYear = `${date.getFullYear() - 1}-${date.getFullYear()}`;
     prevFiscalYear = `${date.getFullYear() - 2}-${date.getFullYear() - 1}`;
   }
