bc0627df714372a7ed81926179d8f1e88ea0fb1d
soab42 <syfuddhin@gmail.com>
2024-09-08 14:41:56 +0600

    sort voucher by date

---
+++ app/(auth)/error.js
@@ -0,0 +1,7 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+export default function error(error) {
+  return <div>Sorry Your credentials are wrong! please try again.</div>;
+}
